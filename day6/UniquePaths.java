b336dabc85a750dab25238fab62c04d5de183afd
Konda Babu <kondababuparasa001@gmail.com>
2026-06-10 06:11:31 +0530

    UniquePaths

---
+++ day6/UniquePaths.java
@@ -0,0 +1,12 @@
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int N = m+n-2;
+        int R = Math.min(m-1,n-1);
+        double result = 1;
+
+        for(int i=1; i<=R; i++){
+            result = result * (N - i + 1)/i;
+        }
+        return (int) result;   
+    }
+}
