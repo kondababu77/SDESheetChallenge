14aed6f86ef70e0cc11c8328a3ba07bfe40a4198
Konda Babu <kondababuparasa001@gmail.com>
2026-06-11 15:49:28 +0530

    LongestSubarrayWithSumK

---
+++ day8/LongestSubarrayWithSumK.java
@@ -0,0 +1,19 @@
+import java.util.*
+public class LongestSubarrayWithSumK {
+    public int longestSubarray(int[] arr, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = 0;
+        map.put(0,-1);
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+            if(map.containsKey(sum-k)){
+                max = Math.max(max, i - map.get(sum-k) );
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
+            }
+        }
+        return max;
+    }
+}
