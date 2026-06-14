ce3b6f83f98b37f669d896ad37616b4145beaace
Parasa Konda Babu <kondababuparasa001@gmail.com>
2026-06-14 06:56:45 +0530

    ReverseList

--- day9/ReverseList.java
+++ day9/ReverseList.java
@@ -1,10 +1,4 @@
-class ListNode {
-     int val;
-     ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
+
 public class ReverseList {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null) return head;
